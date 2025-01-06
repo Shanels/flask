bc9e592d82670ecd93507c1e3d079be306971eb7
Shanels <ananas495@gmail.com>
2025-01-06 17:00:34 +0500

    3

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+from app import app
 from flask import Flask, jsonify
 import os
-
-app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
